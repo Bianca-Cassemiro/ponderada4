96059f8c86586c740738a5e9ede92bb94e6cf00f
biankk <99203402+Bianca-Cassemiro@users.noreply.github.com>
2023-10-02 10:49:03 -0300

    ip elastic

--- frontend/src/pages/dashboard.js
+++ frontend/src/pages/dashboard.js
@@ -50,7 +50,7 @@ const Dashboard = () => {
     };
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/predict", {
+      const response = await fetch("http://34.196.126.149/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
